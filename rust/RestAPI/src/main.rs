c4cace9eab8b2fdf8c18de0283d355bab0ef61a8
msilveus1 <msilveus@wisc.edu>
2021-04-20 19:58:11 -0400

    feat: adding config outline

--- rust/RestAPI/src/main.rs
+++
@@ -1,9 +0,0 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-#[macro_use] extern crate rocket;
-
-#[ path = "services/configuration_service.rs" ] mod configuration_service;
-
-fn main() {
-    rocket::ignite()    
-        .launch();
-}
--- rust/RestAPI/src/services/configuration_service.rs
+++
@@ -1,5 +0,0 @@
-#[ path = "../infrastructure/db.rs"] mod db;
-
-pub fn get_configuration(){
-    // db::get_db_connection()
-}
