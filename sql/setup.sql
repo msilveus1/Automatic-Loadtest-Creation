9631ec8642dd443fa0dc956116c9b4251950f12c
msilveus1 <msilveus@wisc.edu>
2021-04-23 19:07:48 -0400

    feat: adding the config template

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,21 +1,15 @@
 CREATE DATABASE IF NOT EXISTS Loadtest_Automation;
 USE Loadtest_Automation;
 
-CREATE TABLE Load_Test_Servers (
-    server_ip INT UNSIGNED,
-    server_name VARCHAR(128),
-    server_type VARCHAR(64),
-    last_connection_date DATETIME,
-    first_connection_date DATETIME,
-);
-
 CREATE TABLE Load_Test_Definition(
-    test_name VARCHAR(64),
-    line_number INT UNSIGNED,
-    origin_file_path VARCHAR(MAX),
-    origin_repository VARCHAR(MAX),
+    config_id BINARY(16)
     data_type VARCHAR(16),
     data_source VARCHAR(MAX),
     FOREIGN KEY (test_server_ip) REFERENCES Servers(server_ip);
 );
 
+CREATE TABLE DB_Version(
+    version_number INT
+)
+
+INSERT INTO DB_Version (version_number) VALUES(0)
